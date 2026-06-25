13436aa44c4a3757648a874d157983aa7d5da7c0
itecfinder <businessadviserpro@gmail.com>
2026-06-25 10:32:57 -0400

    Remove AppProvider wrapper from Page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import { AppShell } from "@/components/app-shell"
-import { AppProvider } from "@/lib/store"
 import { Toaster } from "@/components/ui/sonner"
 
 export default function Page() {
   return (
-    <AppProvider>
+    <>
       <AppShell />
       <Toaster position="top-center" />
-    </AppProvider>
+    </>
   )
 }
 
